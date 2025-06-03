8c6d62b59e385427fb4ce174eb8c68153a9c3db9
hyojkim2 <skql2548@gmail.com>
2025-06-03 16:44:56 +0900

    :ambulance: 베이스 코드로 교체

--- src/main/java/com/sprint/mission/discodeit/dto/userstatus/UserStatusUpdateRequest.java
+++
@@ -1,8 +0,0 @@
-package com.sprint.mission.discodeit.dto.userstatus;
-
-import java.time.Instant;
-
-public record UserStatusUpdateRequest
-    (Instant time) {
-
-}
--- src/test/java/com/spring/mission/discodeit/service/ChannelServiceTest.java
+++
@@ -1,5 +0,0 @@
-package com.spring.mission.discodeit.service;
-
-public class ChannelServiceTest {
-
-}
