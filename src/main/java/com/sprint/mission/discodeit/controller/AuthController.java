8aeda2ab70ce753b16bd1d566d6cc9d90b9a812b
hyojkim_lt <skql2548@gmail.com>
2025-06-09 15:17:29 +0900

    :sparkles: [sprint9] 기본 인증 구현- 기존 로그인 관련 코드 제거

--- src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
+++ src/main/java/com/sprint/mission/discodeit/controller/AuthController.java
@@ -1,20 +1,14 @@
 package com.sprint.mission.discodeit.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sprint.mission.discodeit.controller.api.AuthApi;
-import com.sprint.mission.discodeit.dto.data.UserDto;
-import com.sprint.mission.discodeit.dto.request.LoginRequest;
 import com.sprint.mission.discodeit.service.AuthService;
-
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,20 +18,11 @@ public class AuthController implements AuthApi {
 
   private final AuthService authService;
 
-
-  // @GetMapping("/csrf-token")
-  // public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
-  //   // Spring Security가 자동으로 CsrfToken을 주입해줍니다
-  //   return ResponseEntity.ok(csrfToken);
-  // }
-
-  @PostMapping(path = "login")
-  public ResponseEntity<UserDto> login(@RequestBody @Valid LoginRequest loginRequest) {
-    log.info("로그인 요청: username={}", loginRequest.username());
-    UserDto user = authService.login(loginRequest);
-    log.debug("로그인 응답: {}", user);
-    return ResponseEntity
-        .status(HttpStatus.OK)
-        .body(user);
+  @GetMapping("/csrf-token")
+  public ResponseEntity<CsrfToken> getCsrfToken(CsrfToken csrfToken) {
+    // Spring Security가 자동으로 CsrfToken을 주입해줍니다
+    return ResponseEntity.ok(csrfToken);
   }
+
+
 }
